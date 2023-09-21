63f6e095161a06f955d8073986c9a12e34609688
BorjaLagarejo <borjalagarejo@gmail.com>
2023-09-22 01:26:14 +0200

    Menu horizontal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { appRoutes } from './app.routing';
+import { NavigationModule } from './components/navigation/navigation.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot(appRoutes),
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        RouterModule.forRoot(appRoutes),
 
-    HttpClientModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+        NavigationModule,
+
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
