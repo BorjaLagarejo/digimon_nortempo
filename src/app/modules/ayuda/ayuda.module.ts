08ddfa60a4c4b921b241e3225847cd75750cbe09
BorjaLagarejo <borjalagarejo@gmail.com>
2023-09-22 10:46:19 +0200

    Zona de ayuda

--- src/app/modules/ayuda/ayuda.module.ts
+++ src/app/modules/ayuda/ayuda.module.ts
@@ -2,14 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AyudaComponent } from './ayuda.component';
 
+import { Route, RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatExpansionModule } from '@angular/material/expansion';
 
+export const ayudaRoute: Route[] = [
+    {
+        path: '',
+        component: AyudaComponent
+    }
+];
 
 @NgModule({
-  declarations: [
-    AyudaComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [AyudaComponent],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(ayudaRoute),
+        MatButtonModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule
+    ]
 })
-export class AyudaModule { }
+export class AyudaModule {}
